e5ac88898105e285063bad586d45a171e011facc
Aditi Divekar <divaaditi101@gmail.com>
2026-05-15 19:04:31 +0530

    debug: log CLIENT_URL on startup

--- server/src/index.js
+++ server/src/index.js
@@ -10,6 +10,10 @@ import pollRoutes from './routes/pollRoutes.js';
 import responseRoutes from './routes/responseRoutes.js';
 
 dotenv.config();
+dotenv.config();
+console.log('CLIENT_URL:', process.env.CLIENT_URL); // debug line
+connectDB();
+
 connectDB();
 
 const app = express();
